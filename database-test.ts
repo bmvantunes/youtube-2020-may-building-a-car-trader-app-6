44be913d264d1229e32d6cbf5e8db361cae8a2a6
Bruno Antunes <bmvantunes@gmail.com>
2020-11-12 11:14:01 +0000

    move the database-test into typescript for people that want/need it in typescript

--- database-test.ts
+++ database-test.ts
@@ -1,13 +1,9 @@
-const sqlite = require('sqlite');
-const sqlite3 = require('sqlite3');
+import { openDB } from './src/openDB';
 
 async function setup() {
-  const db = await sqlite.open({
-    filename: 'cars.sqlite',
-    driver: sqlite3.Database,
-  });
+  const db = await openDB();
 
-  await db.migrate({ force: 'last' });
+  await db.migrate({ force: true });
 
   const faq = await db.all('SELECT * FROM FAQ ORDER BY createDate DESC');
   console.log('ALL faq', JSON.stringify(faq, null, 2));
--- src/openDB.ts
+++ src/openDB.ts
@@ -1,9 +1,9 @@
 import { open } from 'sqlite'
-import sqlite3 from 'sqlite3'
+import { Database } from 'sqlite3'
 
 export async function openDB () {
   return open({
     filename: 'cars.sqlite',
-    driver: sqlite3.Database
+    driver: Database
   })
 }
